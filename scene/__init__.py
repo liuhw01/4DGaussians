fd2e192a1879f5f08f3bd1ed3c8e1054ed306325
Hanwei Liu <67702642+liuhw01@users.noreply.github.com>
2025-06-11 21:30:56 +0800

    Update __init__.py

--- scene/__init__.py
+++ scene/__init__.py
@@ -20,6 +20,20 @@ from arguments import ModelParams
 from utils.camera_utils import cameraList_from_camInfos, camera_to_JSON
 from torch.utils.data import Dataset
 from scene.dataset_readers import add_points
+
+# 这个类是训练过程中整个场景数据的抽象，它负责：
+# 加载场景元数据（相机、点云等）
+# 管理训练 / 测试 / 视频相机列表
+# 初始化或加载高斯模型（GaussianModel）
+# 控制训练过程中点的加载、保存和管理
+
+# | 参数                                | 含义                                  |
+# | --------------------------------- | ----------------------------------- |
+# | `args`                            | 从命令行解析得到的模型参数结构体（如数据路径、是否eval、白背景等） |
+# | `gaussians`                       | 用于表示和管理高斯点云的模型类                     |
+# | `load_iteration`                  | 是否加载已有的点云模型（从某次迭代的 `.ply` 和参数中恢复）   |
+# | `shuffle` / `resolution_scales` 等 | 可选，用于控制数据加载顺序和多分辨率等                 |
+
 class Scene:
 
     gaussians : GaussianModel
@@ -38,7 +52,12 @@ class Scene:
             else:
                 self.loaded_iter = load_iteration
             print("Loading trained model at iteration {}".format(self.loaded_iter))
-
+            
+        # 调用不同的解析器获得：
+        # 点云 (scene_info.point_cloud)
+        # 相机列表（训练/测试/视频）
+        # 归一化信息（半径、中心）
+        # 时间范围（最大时间 maxtime）
         self.train_cameras = {}
         self.test_cameras = {}
         self.video_cameras = {}
@@ -67,6 +86,10 @@ class Scene:
         self.dataset_type = dataset_type
         self.cameras_extent = scene_info.nerf_normalization["radius"]
         print("Loading Training Cameras")
+
+        # 调用 FourDGSdataset 类将每个相机（及其图像）封装为 PyTorch 兼容数据结构：
+        # 支持 __getitem__ 获取相机视角图像、位姿、时间
+        # 支持时间控制、分辨率缩放、多模态信息（深度、mask）
         self.train_camera = FourDGSdataset(scene_info.train_cameras, args, dataset_type)
         print("Loading Test Cameras")
         self.test_camera = FourDGSdataset(scene_info.test_cameras, args, dataset_type)
@@ -81,6 +104,11 @@ class Scene:
             # breakpoint()
             scene_info = scene_info._replace(point_cloud=add_points(scene_info.point_cloud, xyz_max=xyz_max, xyz_min=xyz_min))
         self.gaussians._deformation.deformation_net.set_aabb(xyz_max,xyz_min)
+
+        # 如果没提供 load_iteration，则直接从 scene_info.point_cloud 初始化新的高斯点：
+        #     设置位置、缩放、方向
+        #     时间属性（用于 4D 场景）
+        #     如果提供了 load_iteration（或设置为 -1），则加载已有 .ply 文件和 .pt 权重：
         if self.loaded_iter:
             self.gaussians.load_ply(os.path.join(self.model_path,
                                                            "point_cloud",
